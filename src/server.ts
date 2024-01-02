856e08230aedf60ce7b638b5e027cfe8fa173b7b
Eric Soares Silva <ericsoares51@hotmail.com>
2024-01-02 16:17:25 -0300

    chore: removed chalk

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
-import chalk from 'chalk';
 import { app } from './app';
 import { env } from './env';
 
 app.listen(env.PORT, () =>
-  console.log(chalk.greenBright(`Server is Running on Port ${env.PORT}`)),
+  console.log(`Server is Running on Port ${env.PORT}`),
 );
